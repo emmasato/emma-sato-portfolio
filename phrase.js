e9f457b8e23968d318dfcc640015b89a3fe677a0
emmasato <egraceharris@gmail.com>
2021-12-02 21:52:30 -0800

    update

--- phrase.js
+++ phrase.js
@@ -8,16 +8,16 @@
 var nameArray = [
   "<a class='phrase' href='https://poets.org/poem/revolutionary-letter-2'>looking for answers.</a>",
   "<a class='phrase' href='https://www.youtube.com/watch?v=fzMU2luS7uw'>on the run.</a>",
-  "<a class='phrase' href='#'>packing up her soapbox.</a>",
-  "<a class='phrase' href='#'>having her cake and eating it.</a>",
-  "<a class='phrase' href='#'>commiting a faux paus.</a>",
-  "<a class='phrase' href='#'>engaging serendipity.</a>",
+  "<a class='phrase' href=''>packing up her soapbox.</a>",
+  "<a class='phrase' href=''>having her cake and eating it.</a>",
+  "<a class='phrase' href=''>commiting a faux paus.</a>",
+  "<a class='phrase' href=''>engaging serendipity.</a>",
   "<a class='phrase' href='https://img.buzzfeed.com/buzzfeed-static/static/2017-09/25/6/asset/buzzfeed-prod-fastlane-03/anigif_sub-buzz-24369-1506334088-1.gif'>guessing, and getting it right.</a>",
   "<a class='phrase' href='https://www.youtube.com/watch?v=ZFq_Ib8BkVI'>preaching to the choir.</a>",
   "<a class='phrase' href='https://fontsinuse.com/'>keeping tabs.</a>",
-  "<a class='phrase' href='#'>believing the best.</a>",
-  "<a class='phrase' href='#'>taking drastic measures.</a>",
-  "<a class='phrase' href='#'>trusting her gut.</a>",
+  "<a class='phrase' href=''>believing the best.</a>",
+  "<a class='phrase' href=''>taking drastic measures.</a>",
+  "<a class='phrase' href=''>trusting her gut.</a>",
   "<a class='phrase' href='https://www.merriam-webster.com/dictionary/non%20sequitur'>catching non sequiturs.</a>",
 
 ]; //One of those will be randomly chosen
